7c8b95dec51ebfab078d4158637a57f982f7bd44
Duncan Walter <duncan1997@hotmail.nl>
2025-09-17 13:48:02 +0200

    Added SuperMarket class

---
+++ src/SuperMarket.java
@@ -0,0 +1,13 @@
+public class SuperMarket {
+    public Product bread;
+    public Product fruit;
+    public Product toiletPaper;
+    public Product cheese;
+
+    public SuperMarket(Product bread, Product fruit, Product toiletPaper, Product cheese) {
+        this.bread = bread;
+        this.fruit = fruit;
+        this.toiletPaper = toiletPaper;
+        this.cheese = cheese;
+    }
+}
