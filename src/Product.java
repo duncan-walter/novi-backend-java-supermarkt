8ba88b7fc99a8b71fdaa83ed81891ea927c04be2
Duncan Walter <duncan1997@hotmail.nl>
2025-09-17 13:47:46 +0200

    Added Product class

---
+++ src/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    public String name;
+    public float price;
+    public short amount;
+
+    public Product(String name, float price, short amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+}
