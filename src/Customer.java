8bed7d098d8fdf94142d7c7cfba6e4fce68970c4
Duncan Walter <duncan1997@hotmail.nl>
2025-09-17 14:42:47 +0200

    Implemented buyItem method on Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -9,4 +9,18 @@ public class Customer {
     public void goToSuperMarket(SuperMarket superMarket) {
         this.superMarket = superMarket;
     }
+
+    public void buyItem(String productName, short amount) {
+        if (superMarket.bread.name == productName) {
+            superMarket.buyBread(amount);
+        } else if (superMarket.fruit.name == productName) {
+            superMarket.buyFruit(amount);
+        } else if (superMarket.toiletPaper.name == productName) {
+            superMarket.buyToiletPaper(amount);
+        } else if (superMarket.cheese.name == productName) {
+            superMarket.buyCheese(amount);
+        } else {
+            System.out.printf("%s does not exist in this super market.", productName);
+        }
+    }
 }
